e4413227a6ed97be6118a55bd0a7c7ba6a64f2bb
datletu <52278059+datletu@users.noreply.github.com>
2025-01-23 22:34:31 +0700

    Level 1. Echo

--- src/main/java/Kunkka.java
+++ src/main/java/Kunkka.java
@@ -4,7 +4,14 @@ public class Kunkka {
         String name = "Kunkka";
         String greeting = "Ahoy! I'm " + name + "\n" + "What can I do for you?";
         String farewell = "Farewell, matey! May the wind be at your back!";
-        String output = horizontalLine + "\n" + greeting + "\n" + horizontalLine + "\n" + farewell + "\n" + horizontalLine;
-        System.out.println(output);
+        System.out.println(horizontalLine + "\n" + greeting + "\n" + horizontalLine + "\n");
+
+        String input = new java.util.Scanner(System.in).nextLine();
+        while (!input.equals("bye")) {
+            String output = horizontalLine + "\n" + input + "\n" + horizontalLine + "\n";
+            System.out.println(output);
+            input = new java.util.Scanner(System.in).nextLine();
+        }
+        System.out.println(horizontalLine + "\n" + farewell + "\n" + horizontalLine);
     }
 }
