53bde4f8a9ecec6afa83ab4a3593ae1941ddd53a
datletu <52278059+datletu@users.noreply.github.com>
2025-01-23 22:27:28 +0700

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Kunkka.java
@@ -0,0 +1,10 @@
+public class Kunkka {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+        String name = "Kunkka";
+        String greeting = "Ahoy! I'm " + name + "\n" + "What can I do for you?";
+        String farewell = "Farewell, matey! May the wind be at your back!";
+        String output = horizontalLine + "\n" + greeting + "\n" + horizontalLine + "\n" + farewell + "\n" + horizontalLine;
+        System.out.println(output);
+    }
+}
