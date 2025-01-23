7edc194cd725a549ea53f70e6a0d01004705f07d
datletu <52278059+datletu@users.noreply.github.com>
2025-01-23 23:12:30 +0700

    Level 2. Add, List and add a logo

--- src/main/java/Kunkka.java
+++ src/main/java/Kunkka.java
@@ -1,17 +1,47 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Kunkka {
     public static void main(String[] args) {
+        String logo = " _______  ______   _______ _________ _______  _______  _          _                 _        _        _        _______ \n"
+                    + "(  ___  )(  __  \\ (       )\\__   __/(  ____ )(  ___  )( \\        | \\    /\\|\\     /|( (    /|| \\    /\\| \\    /\\(  ___  )\n"
+                    + "| (   ) || (  \\  )| () () |   ) (   | (    )|| (   ) || (        |  \\  / /| )   ( ||  \\  ( ||  \\  / /|  \\  / /| (   ) |\n"
+                    + "| (___) || |   ) || || || |   | |   | (____)|| (___) || |        |  (_/ / | |   | ||   \\ | ||  (_/ / |  (_/ / | (___) |\n"
+                    + "|  ___  || |   | || |(_)| |   | |   |     __)|  ___  || |        |   _ (  | |   | || (\\ \\) ||   _ (  |   _ (  |  ___  |\n"
+                    + "| (   ) || |   ) || |   | |   | |   | (\\ (   | (   ) || |        |  ( \\ \\ | |   | || | \\   ||  ( \\ \\ |  ( \\ \\ | (   ) |\n"
+                    + "| )   ( || (__/  )| )   ( |___) (___| ) \\ \\__| )   ( || (____/\\  |  /  \\ \\| (___) || )  \\  ||  /  \\ \\|  /  \\ \\| )   ( |\n"
+                    + "|/     \\|(______/ |/     \\|\\_______/|/   \\__/|/     \\|(_______/  |_/    \\/(_______)|/    )_)|_/    \\/|_/    \\/|/     \\|\n";
+        System.out.println(logo);
         String horizontalLine = "____________________________________________________________";
         String name = "Kunkka";
         String greeting = "Ahoy! I'm " + name + "\n" + "What can I do for you?";
         String farewell = "Farewell, matey! May the wind be at your back!";
         System.out.println(horizontalLine + "\n" + greeting + "\n" + horizontalLine + "\n");
 
-        String input = new java.util.Scanner(System.in).nextLine();
+        String input = new Scanner(System.in).nextLine();
+
+        List<String> tasks = new ArrayList<String>();
+
         while (!input.equals("bye")) {
-            String output = horizontalLine + "\n" + input + "\n" + horizontalLine + "\n";
-            System.out.println(output);
-            input = new java.util.Scanner(System.in).nextLine();
+            System.out.println(horizontalLine + "\n");
+
+            if (input.equals("list")) {
+                printTasks(tasks);
+            } else {
+                tasks.add(input);
+                System.out.println("added: " + input);
+            }
+
+            System.out.println(horizontalLine + "\n");
+            input = new Scanner(System.in).nextLine();
         }
         System.out.println(horizontalLine + "\n" + farewell + "\n" + horizontalLine);
+    }
+
+    static void printTasks(List<String> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
     }
 }
