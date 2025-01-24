9ce41074fc99de5e49ea8b73ed283b3f4340b1ca
datletu <52278059+datletu@users.noreply.github.com>
2025-01-24 10:42:50 +0700

    Level 3. Mark as Done

--- src/main/java/Kunkka.java
+++ src/main/java/Kunkka.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import components.Task;
 
 public class Kunkka {
     public static void main(String[] args) {
-        
         String logo = " _______  ______   _______ _________ _______  _______  _          _                 _        _        _        _______ \n"
                     + "(  ___  )(  __  \\ (       )\\__   __/(  ____ )(  ___  )( \\        | \\    /\\|\\     /|( (    /|| \\    /\\| \\    /\\(  ___  )\n"
                     + "| (   ) || (  \\  )| () () |   ) (   | (    )|| (   ) || (        |  \\  / /| )   ( ||  \\  ( ||  \\  / /|  \\  / /| (   ) |\n"
@@ -21,26 +21,49 @@ public class Kunkka {
         System.out.println(horizontalLine + "\n" + greeting + "\n" + horizontalLine + "\n");
 
         String input = new Scanner(System.in).nextLine();
+        Task task = new Task(input);
+        List<Task> tasks = new ArrayList<Task>();
 
-        List<String> tasks = new ArrayList<String>();
-
-        while (!input.equals("bye")) {
+        while (!task.getName().equals("bye")) {
             System.out.println(horizontalLine + "\n");
 
-            if (input.equals("list")) {
+            //Handle list command
+            if (task.getName().equals("list")) {
                 printTasks(tasks);
-            } else {
-                tasks.add(input);
-                System.out.println("added: " + input);
+            } 
+
+            //Handle mark command
+            else if (task.getName().matches("mark \\d+")) {
+                int index = Integer.parseInt(input.split(" ")[1]);
+                tasks.get(index - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks.get(index - 1));
+            }
+
+            //Handle unmark command
+            else if (task.getName().matches("unmark \\d+")) {
+                int index = Integer.parseInt(input.split(" ")[1]);
+                tasks.get(index - 1).unmarkAsDone();
+                System.out.println("Nice! I've unmarked this task:");
+                System.out.println("  " + tasks.get(index - 1));
+            }
+
+            //Handle adding tasks
+            else {
+                tasks.add(task);
+                System.out.println("added: " + task);
             }
 
             System.out.println(horizontalLine + "\n");
             input = new Scanner(System.in).nextLine();
+            task = new Task(input);
         }
+
+        //Handle bye command
         System.out.println(horizontalLine + "\n" + farewell + "\n" + horizontalLine);
     }
 
-    static void printTasks(List<String> tasks) {
+    static void printTasks(List<Task> tasks) {
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println((i + 1) + ". " + tasks.get(i));
         }
---
+++ src/main/java/components/Task.java
@@ -0,0 +1,31 @@
+package components;
+public class Task {
+    protected String name;
+    protected boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        this.isDone = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + name;
+    }
+}
